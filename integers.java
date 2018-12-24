9ffb92a509cdee1e03a5acef1986f1d002d2bf9e
root <root@devops1>
2018-12-24 12:00:19 +0000

    / included

--- integers.java
+++ integers.java
@@ -1,7 +1,9 @@
 class Integers {
    public static void main (String [] arguments) {
         int c; //declaring a variable
-   /* using for loop to repeat instruction execution *
+
+   /* using for loop to repeat instruction execution */
+
         for (c = 1; c <= 10; c++) {
           system.out.println(c);
       }
