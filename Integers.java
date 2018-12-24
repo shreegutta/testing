30709371cca9addf101430db2d63ca401e3fc457
root <root@devops1>
2018-12-24 12:09:29 +0000

    name capital added

---
+++ Integers.java
@@ -0,0 +1,15 @@
+class Integers {
+   public static void main (String [] arguments) {
+        int c; //declaring a variable
+
+
+   //* using for loop to repeat instruction execution *//
+
+        for (c = 1; c <= 10; c++) {
+          system.out.println(c);
+      }
+      system.out.println("End of for loop - All the best to Shree");
+      system.out.println("This is contionous deployment testing");
+   }
+}
+
